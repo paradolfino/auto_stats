1e26e645bc04ad85cf66fd8edf33d1f997545598
Viktharien <viktharien@zoho.com>
2018-04-17 21:27:22 +0000

    set up actions, private methods for transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,15 @@
 class TransactionsController < ApplicationController
+    def new
+        
+    end
+    
+    def increment
+        
+    end
+    
+    private
+        
+        def transaction_params
+           params.require(:transaction).permit(:name, :count) 
+        end
 end
