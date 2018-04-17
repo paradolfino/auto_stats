c9e7c7c5ffeb86ca65aba37f678c204568bf19b7
Viktharien <viktharien@zoho.com>
2018-04-17 21:22:05 +0000

    generate transaction model

--- db/migrate/20180417212127_create_transactions.rb
+++ db/migrate/20180417212127_create_transactions.rb
@@ -1,7 +1,8 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-
+      t.string :name
+      t.integer :count
       t.timestamps
     end
   end
