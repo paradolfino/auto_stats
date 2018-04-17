4274d2e58ab0d65789ba1dfdd93bda3eb73ebd75
Viktharien <viktharien@zoho.com>
2018-04-17 21:30:07 +0000

    set up actions, private methods for transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_17_212127) do
-
-  create_table "transactions", force: :cascade do |t|
-    t.string "name"
-    t.integer "count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
