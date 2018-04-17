fb2cdc200623032b429611f403869b8f26ff1c1e
Viktharien <viktharien@zoho.com>
2018-04-17 21:30:17 +0000

    set up actions, private methods for transactions

--- db/migrate/20180417212127_create_transactions.rb
+++ db/migrate/20180417212127_create_transactions.rb
@@ -2,7 +2,6 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.string :name
-      t.integer :count
       t.timestamps
     end
   end
