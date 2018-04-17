ce9c4ba8abf63f9c9a4565fa6455678fa9fa16a0
Viktharien <viktharien@zoho.com>
2018-04-17 21:46:58 +0000

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
     root 'transactions#index'
     get '/trainings' => 'transactions#trainings'
-    post '/trainings' => ''
+    post '/trainings' => 'transactions#create'
 end
